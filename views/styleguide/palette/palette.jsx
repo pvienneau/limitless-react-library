afe82e156ece16e78308fabfbb7794b1fa44cc88
Patrick Vienneau <pvien027@gmail.com>
2018-01-28 22:27:07 -0500

    Completes colors and palette library

--- views/styleguide/palette/palette.jsx
+++ views/styleguide/palette/palette.jsx
@@ -6,32 +6,49 @@ import classNames from 'classnames';
 import { Container, Card } from 'components';
 import './palette.scss';
 
-const COLORS = ['primary', 'danger', 'warning', 'info', 'success'];
+const PRIMARY_COLORS = ['primary', 'danger', 'warning', 'info', 'success'];
+const SECONDARY_COLORS = ['pink', 'violet', 'purple', 'indigo', 'blue', 'teal', 'green', 'orange', 'brown', 'grey', 'slate'];
 const GRADES = [800, 700, 600, 500, 400, 300];
 
 export default () => (
   <Container className="Palette">
     {
-      COLORS.map(color => (
-        <div
-          key={color}
-          className={classNames('color-spectrum', `color-spectrum-${color}`)}
-        >
+      [
+        {
+          title: 'Primary colors',
+          colors: PRIMARY_COLORS,
+        },
+        {
+          title: 'Secondary colors',
+          colors: SECONDARY_COLORS,
+        },
+      ].map(({ title, colors }) => (
+        <section>
+          <h6>{title}</h6>
           {
-            GRADES.map(grade => (
+            colors.map(color => (
               <div
-                key={grade}
-                className={classNames('color-grade', `color-${color}-${grade}`)}
+                key={color}
+                className={classNames('color-spectrum', `color-spectrum-${color}`)}
               >
-                <Card
-                  title={`${capitalize(color)} ${grade}`}
-                  description={'lorem ipsum.'}
-                  hintText={`${color}-${grade}`}
-                />
+                {
+                  GRADES.map(grade => (
+                    <div
+                      key={grade}
+                      className={classNames('color-grade', `color-${color}-${grade}`)}
+                    >
+                      <Card
+                        title={`${capitalize(color)} ${grade}`}
+                        description={'lorem ipsum.'}
+                        hintText={`${color}-${grade}`}
+                      />
+                    </div>
+                  ))
+                }
               </div>
             ))
           }
-        </div>
+        </section>
       ))
     }
   </Container>
